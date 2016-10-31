35a575421ad848c0350c56832f098fea355d6390
CristianGVera <cristiang.vera@yahoo.com.ar>
2016-10-30 22:16:58 -0300

    Grafo aleatorio dado N y porcentaje de Adyacencia

---
+++ TPs/TP4-Coloreo de grafos/src/grafos/GenAlNProbAdyacencia.java
@@ -0,0 +1,42 @@
+package grafos;
+
+import java.util.Random;
+
+public class GenAlNProbAdyacencia {
+	private int cantAristas;
+	private int cantNodos;
+	private int porcAdyacencia;
+	private MatrizSimetrica matriz;
+	
+	public GenAlNProbAdyacencia(int cantNodos, int porcAdyacencia)
+	{
+		this.cantNodos = cantNodos;
+		matriz = new MatrizSimetrica(cantNodos);
+		this.porcAdyacencia = porcAdyacencia;
+		this.cantAristas = (porcAdyacencia / 100) * ((this.cantNodos * (this.cantNodos - 1)) / 2);
+	}
+	
+	public void GrafoAlNProbAdyacencia() {
+		Random r = new Random(2);
+		int aristas = 0;
+		
+		while(aristas != this.cantAristas)
+		{
+			for(int i = 0; i < cantNodos; i++)
+			{
+				for(int j = cantNodos; i < j; j--)
+				{
+					if(r.nextInt() == 1)
+					{
+						matriz.setDato(i, j);
+						aristas++;
+					}
+					if(aristas == cantAristas)
+						break;
+				}
+				if(aristas == cantAristas)
+					break;
+			}
+		}
+	}
+}
